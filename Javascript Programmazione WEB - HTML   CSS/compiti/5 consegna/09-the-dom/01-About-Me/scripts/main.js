979a8396ff709821e77ee09b8baefc5da010fb21
brian potosi ccuno <brian_xpg@hotmail.com>
2022-06-11 13:39:39 +0200

    lala

--- Javascript Programmazione WEB - HTML   CSS/compiti/5 consegna/09-the-dom/01-About-Me/scripts/main.js
+++ Javascript Programmazione WEB - HTML   CSS/compiti/5 consegna/09-the-dom/01-About-Me/scripts/main.js
@@ -1,12 +1,14 @@
-let body = document.querySelector('body');
-body.style.fontFamily = 'Arial, sans-serif';
+let body = document.querySelector('body')
+body.style.fontFamily = 'Arial, sans-serif'
+body.style.backgroundColor="red"
+console.log(Nickname.innerHTML);
 
-let Nickname=document.querySelector('li #nickname');
-Nickname.innerHTML+='Briancito';
-let Favorites=document.querySelector('li #favorites');
-Favorites.innerHTML+='games';
-let Hometown=document.querySelector('li #hometown');
-Hometown.innerHTML+='Turin';
+let Nickname=document.getElementById('nickname')
+Nickname.innerHTML+='Briancito'
+let Favorites=document.getElementById('favorites')
+Favorites.innerHTML+='games'
+let Hometown=document.getElementById('hometown')
+Hometown.innerHTML+='Turin'
 
 let classes=document.querySelector('li')
 classes.className('list-item');
